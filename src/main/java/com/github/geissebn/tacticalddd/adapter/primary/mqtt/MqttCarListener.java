474dd0c0e9611417627efc574e3785ea13192c6a
Benjamin Geisselmeier <geissebn@users.noreply.github.com>
2024-01-15 15:54:07 +0100

    Improve MQTT subscription

--- src/main/java/com/github/geissebn/tacticalddd/adapter/primary/mqtt/MqttCarListener.java
+++ src/main/java/com/github/geissebn/tacticalddd/adapter/primary/mqtt/MqttCarListener.java
@@ -7,8 +7,6 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.slf4j.MDC;
@@ -34,23 +32,7 @@ public class MqttCarListener {
 
     @PostConstruct
     void connectToTopic() throws MqttException {
-        mqttClient.subscribe(topic);
-        mqttClient.setCallback(new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
-
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) {
-                MqttCarListener.this.handleMessage(topic, message);
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-
-            }
-        });
+        mqttClient.subscribe(topic, this::handleMessage);
     }
 
     private void handleMessage(String topic, MqttMessage message) {
